2695e8580ef539761d77bf0f4fcb4b9dbf161ea2
adojem <adojemcovix@gmail.com>
2018-08-27 19:07:29 +0900

    Install and Setup Eloquent ORM

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -4,6 +4,9 @@
 
    // Load environment variables
    require_once __DIR__ . '/../app/config/_env.php';
-   require_once __DIR__ . '/../app/routing/routes.php';
+   // Instantiate database class
+   new \App\Classes\Database();
 
+   require_once __DIR__ . '/../app/routing/routes.php';
+   
    new \App\RouteDispatcher($router);
--- public/index.php
+++ public/index.php
@@ -2,5 +2,9 @@
    require_once __DIR__ . '/../bootstrap/init.php';
 
    $app_name = getenv('APP_NAME');
+
+   use illuminate\Database\Capsule\Manager as Capsule;
   
-  
+  $user = Capsule::table('categories')->get();
+
+  var_dump($user->toArray());
