372dee7df898a9e9a9bfe5e1d59e928f17d357ce
adojem <adojemcovix@gmail.com>
2018-09-05 20:49:40 +0900

    Creating a CSRF Protection Class

---
+++ app/classes/CSRFToken.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Classes;
+
+class CSRFToken {
+
+   public static function _token() {
+      if (!Session::has('token')) {
+         $randomToken = base64_encode(openssL_random_pseudo_bytes(32));
+         Session::add('token', $randomToken);
+      }
+
+      return Session::get('token');
+   }
+
+   /**
+    * Verify CSRG TOKEN
+    * @param $requestToken
+    * @return bool
+    */
+   public static function verifyCSRFToken($requestToken) {
+      if (Session::has('token') && Session::get('token') === '$requestToken') {
+         Session::remove('token');
+         return true;
+      }
+      return false;
+   }
+}
