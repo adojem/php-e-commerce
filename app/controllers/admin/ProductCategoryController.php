3f05a1f2c12a6e3ae5306d5379c4191c7c505df3
adojem <adojemcovix@gmail.com>
2018-09-16 11:38:02 +0900

    Form Request Validation - Part 1

--- app/controllers/admin/ProductCategoryController.php
+++ app/controllers/admin/ProductCategoryController.php
@@ -3,12 +3,22 @@ namespace App\Controllers\Admin;
 
 use App\Classes\CSRFToken;
 use App\Classes\Request;
+use App\Classes\ValidateRequest;
 use App\Models\Category;
 
 class ProductCategoryController {
 
    public function show() {
       $categories = Category::all();
+      $data = ValidateRequest::maxLength('name', $request->name, 3);
+
+      if ($data) {
+         echo "All good"; exit;
+      }
+      else {
+         echo "Min lengthh is 6"; exit;
+      }
+
       
       return view('admin/products/categories', compact('categories'));
    }
