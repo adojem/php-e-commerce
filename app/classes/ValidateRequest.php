5d055624562f2cda3fb7822de5ec851bd7563a76
adojem <adojemcovix@gmail.com>
2018-10-09 17:33:56 +0900

    Form Request Validation - Part 4

--- app/classes/ValidateRequest.php
+++ app/classes/ValidateRequest.php
@@ -6,6 +6,23 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class ValidateRequest {
 
+   private static $error = [];
+   private static $error_messages = [
+      'string' => 'The :attribute field cannot contain numbers',
+      'required' => 'The :attribute field is required',
+      'minLength' => 'The :attribute field must be a minimm of :policy characters',
+      'maxLength' => 'The :attribute field must be a minimm of :policy characters',
+      'mixed' => 'The :attribute field can contain letters, numbers, dash and space only',
+      'number' => 'The :attribute field cannot  contain letters e.g. 20.0, 20',
+      'email' => 'Email address is not valid',
+      'unique' => 'That :attribute is already taken, please try another one',
+   ];
+
+   /**
+    * @param array $dataAndValues = column and value to validate
+    * @param array $policies = rules that validation is satisfied
+    * @return void
+    */
    public function abide(array $dataAndValues, array $policies) {
       foreach ($dataAndValues as $column => $value) {
          if (in_array($column, array_keys($policies))) {
@@ -21,10 +38,24 @@ class ValidateRequest {
       }
    }
 
+   /**
+    * Perform validation for the data provider and set error messages
+    *
+    * @param array $data
+    * @return void
+    */
    private static function doValidation(array $data) {
       $column = $data['column'];
       foreach ($data['policies'] as $rule => $policy) {
          $valid = call_user_func_array([self::class, $rule], [$column, $data['value'], $policy]);
+         if (!$valid) {
+            self::setError(
+               str_replace(
+                  [':attribute', ':policy', '_'],
+                  [$column, $policy, ' '],
+                  self::$error_messages[$rule]), $column
+            );
+         }
       }
    }
    
@@ -93,5 +124,30 @@ class ValidateRequest {
          }
       }
       return true;
+   }
+
+   /**
+    * Set specific error
+    */
+   private static function setError($error, $key = null) {
+      if ($key) {
+         self::$error[$key][] = $error;
+      }
+      else {
+         self::$error[] = $error;
+      }
+   }
+
+   /**
+    * return true if there is vlidation error
+    *
+    * @return boolean
+    */
+   public function hasError() {
+      return count(self::$error) > 0 ? true : flase;
+   }
+
+   public function getErrorMessages() {
+      return self::$error;
    }
 }
