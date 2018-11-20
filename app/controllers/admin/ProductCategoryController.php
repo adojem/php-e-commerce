b965ed62710c1b26f543cc8ad21545d506c2e457
adojem <adojemcovix@gmail.com>
2018-11-20 18:38:46 +0900

    Setup Route and Controller Method for Edit Category

--- app/controllers/admin/ProductCategoryController.php
+++ app/controllers/admin/ProductCategoryController.php
@@ -47,7 +47,7 @@ class ProductCategoryController {
 
             if ($validate->hasError()) {
                $errors = $validate->getErrorMessages();
-               // print_r($errors);
+
                return view('admin/products/categories', [
                   'categories' => $this->categories,
                   'links' => $this->links,
@@ -69,6 +69,42 @@ class ProductCategoryController {
                'links' => $this->links,
                'success' => 'Category Created'
             ]);
+         }
+         throw new \Exception('Token mismtach');
+      }
+
+      return null;
+   }
+
+   public function edit($id)
+   {
+      if (Request::has('post')) {
+         $request = Request::get('post');
+
+         if (CSRFToken::verifyCSRFToken($request->token)) {
+            $rules = [
+               'name' => [
+                  'required' => true,
+                  'minLength' => 3,
+                  'string' => true,
+                  'unique' => 'categories',
+               ]
+            ];
+
+            $validate = new ValidateRequest;
+            $validate->abide($_POST, $rules);
+
+            if ($validate->hasError()) {
+               $errors = $validate->getErrorMessages();
+               header('HTTP/1.1 422 Unprocessible Entity', true, 422);
+               echo \json_encode($errors);
+               exit;
+            }
+
+            Category::where('id', $id)->update(['name' => $request->name]);
+            echo \json_encode(['success' => 'Record Update Successfully']);
+            exit;
+            
          }
          throw new \Exception('Token mismtach');
       }
