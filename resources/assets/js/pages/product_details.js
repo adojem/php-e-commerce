df36434631aa5e2189ace53d0269a5812edca735
adojem <adojemcovix@gmail.com>
2018-12-22 13:39:54 +0900

    Product Details Page

--- resources/assets/js/pages/product_details.js
+++ resources/assets/js/pages/product_details.js
@@ -1,14 +1,42 @@
 import Vue from 'vue';
+import axios from 'axios';
 
 const productDetails = () => {
    const app = new Vue({
       el: '#product',
+
       data: {
          product: [],
          category: [],
          subCategory: [],
          productId: document.getElementById('product').dataset.id,
          loading: false,
+      },
+
+      methods: {
+         getProductDetails() {
+            this.loading = true;
+            setTimeout(() => {
+               axios.get(`/product-details/${this.productId}`).then((response) => {
+                  app.product = response.data.product;
+                  app.category = response.data.category;
+                  app.subCategory = response.data.subCategory;
+                  app.loading = false;
+               });
+            }, 1000);
+         },
+      },
+
+      created() {
+         this.getProductDetails();
+      },
+
+      stringLimit(string, value) {
+         if (string.length > value) {
+            return `${string.substring(0, value)}...`;
+         }
+
+         return string;
       },
    });
 };
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,31 +4,53 @@
 
 @section('content')
 
-   <div class="product" id="product" data-token="{{ $token }}" data-id={{ $product->id }} style="padding:6rem">
+   <div class="product" id="product" data-token="{{ $token }}" data-id={{ $product->id }}>
 
-      <div class="text-center">
-         <i v-show="loading" class="fas fa-spinner fa-spin" style="margin-bottom:3rem; color:#0a2b1d; font-size:3rem"></i>
+      <div v-show="loading" class="text-center">
+         <i class="fas fa-spinner fa-spin" style="margin-bottom:3rem; color:#0a2b1d; font-size:3rem"></i>
       </div>
 
-      <section class="grid-x">
+      <section v-if="loading == false">
       
-         <div class="cell">
-            <nav aria-label="You are here:" role="navigation">
-               <ul class="breadcrumbs">
-                  <li><a href="#">Product Category</a></li>
-                  <li><a href="#">Product Subcategory</a></li>
-                  <li class="disabled">Product Name</li>
-               </ul>
-            </nav>
+         <div class="grid-x">
+
+            <div class="cell">
+               <nav aria-label="You are here:" role="navigation">
+                  <ul class="breadcrumbs">
+                     <li>
+                        <a :href="'/product/category/' + category.slug">
+                           @{{ category.name }}
+                        </a>
+                     </li>
+                     <li>
+                        <a :href="'/product/subcategory/' + subCategory.slug">
+                           @{{ subCategory.name }}
+                        </a>
+                     </li>
+                     <li>@{{ product.name }}</li>
+                  </ul>
+               </nav>
+            </div>
+
          </div>
 
-         <div class="grid-x collapse">
+         <div class="grid-x align-middle">
+
             <div class="cell small-12 medium-5 large-4">
-            
+               <div>
+                  <img :src="'/' + product.image_path" :alt=" product.name" width="100%">
+               </div>
             </div>
+
             <div class="cell small-12 medium-7 large-8">
-            
+               <div class="product-details">
+                  <h2>@{{ product.name }}</h2>
+                  <p>@{{ product.description }}</p>
+                  <h2>@{{ product.price }}</h2>
+                  <button class="button alert">Add to Cart</button>
+               </div>
             </div>
+
          </div>
       
       </section>
