dbc210573423daf4713946cab55e28b5f6a3f298
adojem <adojemcovix@gmail.com>
2018-12-28 15:22:29 +0900

    User Registration Functionality

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\Controllers;
 
-use App\Classes\Mail;
 use App\Classes\Redirect;
 use App\Classes\CSRFToken;
 use App\Classes\Request;
-use App\Models\Product;
+use App\Classes\ValidateRequest;
+use App\Models\User;
+use Exception;
+
 
 class AuthController extends BaseController
 {
@@ -21,7 +23,69 @@ class AuthController extends BaseController
 
    public function register()
    {
-      
+      if (Request::has('post')) {
+         $request = Request::get('post');
+
+         if (CSRFToken::verifyCSRFToken($request->token)) {
+            $rules = [
+               'username' => [
+                  'required'  => true,
+                  'maxLength' => 20,
+                  'string'    => true,
+                  'unique'    => 'users',
+               ],
+               'email' => [
+                  'required' => true,
+                  'email'    => true,
+                  'unique'   => 'users',
+               ],
+               'password' => [
+                  'required'  => true,
+                  'minLength' => 6,
+               ],
+               'fullname' => [
+                  'required'  => true,
+                  'minLength' => 6,
+                  'maxLength' => 50,
+               ],
+               'address' => [
+                  'required'  => true,
+                  'minLength' => 4,
+                  'maxLength' => 500,
+                  'mixed'     => true,
+               ]
+            ];
+
+            $validate = new ValidateRequest;
+            $validate->abide($_POST, $rules);
+
+            if ($validate->hasError()) {
+               $errors = $validate->getErrorMessages();
+
+               return view('register', ['errors' => $errors]);
+            }
+
+            // insert into database
+            User::create([
+               'username' => $request->username,
+               'email'    => $request->email,
+               'password' => password_hash($request->password, PASSWORD_BCRYPT),
+               'fullname' => $request->fullname,
+               'address'  => $request->address,
+               'role'     => 'user',
+            ]);
+
+            Request::refresh();
+
+            return view('register', [
+               'success' => 'Account created, please login'
+            ]);
+         }
+
+         throw new Exception('Token Mismatch');
+      }
+
+      return null;
    }
 
    public function login()
